c318dbbae135b2cd55ca330ceb18dae84d9f25f1
daniel <danielvillota1016@gmail.com>
2025-07-11 01:44:31 -0500

    add main page, base template and styles

--- src/frontend/app/(main)/page.tsx
+++ src/frontend/app/(main)/page.tsx
@@ -1,23 +1,70 @@
 import Image from "next/image";
+import { MenuIcon, ShoppingCartIcon } from "lucide-react";
 import styles from "@/css/main/main.module.css";
 import headerStyles from "@/css/main/header.module.css";
+import optionsStyles from "@/css/main/options.module.css";
+import productsStyles from "@/css/main/products.module.css";
+import footerStyles from "@/css/main/footer.module.css";
 
 export default function MainPage() {
   return (
     <div className={styles.container}>
-      <header>
-        <div className={headerStyles.headerImage}>
+      <header className={styles.grid}>
+        <div className={styles.sideItem}>
+          <MenuIcon />
+        </div>
+        <div className={headerStyles.header}>
           <Image
-            src="placeholder.svg"
+            src="/placeholder.svg"
             alt="header-image"
             width="100"
             height="100"
           />
         </div>
-        <div className={headerStyles.headerRight}></div>
+        <div className={styles.sideItem}>
+          <ShoppingCartIcon />
+        </div>
       </header>
-      <main>This is the main component</main>
-      <footer>This is the footer</footer>
+      <main>
+        <div className={styles.grid}>
+          <div />
+          <div className={optionsStyles.container}>
+            <div className={optionsStyles.logo}>M</div>
+          </div>
+          <div />
+        </div>
+        <div className={styles.grid}>
+          <div />
+          <div className={productsStyles.grid}>
+            <div className={productsStyles.card}>
+              <div className={productsStyles.imageContainer}>
+                <Image
+                  src={"/placeholder.svg"}
+                  alt={"placeholder"}
+                  width={300}
+                  height={300}
+                />
+              </div>
+            </div>
+            <div className={productsStyles.card}>
+              <div className={productsStyles.imageContainer}>
+                <Image
+                  src={"/placeholder.svg"}
+                  alt={"placeholder"}
+                  width={300}
+                  height={300}
+                />
+              </div>
+            </div>
+          </div>
+          <div />
+        </div>
+      </main>
+      <footer className={`${footerStyles.footer} ${styles.grid}`}>
+        <div />
+        <div className={footerStyles.content}>© Mystic Madness</div>
+        <div />
+      </footer>
     </div>
   );
 }
